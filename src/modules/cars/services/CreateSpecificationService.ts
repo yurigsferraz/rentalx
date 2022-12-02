b21f9d293099631461eaa0fdcf1c5a3cbc4f6400
Yuri Ferraz <yurigsferraz@gmail.com>
2022-12-02 19:14:33 -0300

    Criando repositório de especificação
    Junto com a sua rota e endpoint de post

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,24 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
 class CreateSpecificationService {
-    execute(): void {
-        console.log("TODO");
+    constructor(private specificationRepository: ISpecificationsRepository) {}
+    execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists =
+            this.specificationRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error("Specification already exists!");
+        }
+
+        this.specificationRepository.create({
+            name,
+            description,
+        });
     }
 }
 
